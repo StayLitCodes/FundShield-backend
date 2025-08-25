7e8dcacd4343edda92a0c0012443e17b10257f5f
shamoo53 <aliyushamsu16@gmail.com>
2025-08-25 11:08:04 +0000

    test vadiation file

--- src/configuration/tests/feature-flag.service.spec.ts
+++ src/configuration/tests/feature-flag.service.spec.ts
@@ -1,22 +1,22 @@
-// import { FeatureFlagService } from '../feature-flag.service';
-// import { ConfigurationService } from '../configuration.service';
+import { FeatureFlagService } from '../feature-flag.service';
+import { ConfigurationService } from '../configuration.service';
 
-// describe('FeatureFlagService', () => {
-//   let configService: ConfigurationService;
-//   let service: FeatureFlagService;
+describe('FeatureFlagService', () => {
+  let configService: ConfigurationService;
+  let service: FeatureFlagService;
 
-//   beforeEach(() => {
-//     configService = { get: jest.fn(() => 'A,B,C') } as any;
-//     service = new FeatureFlagService(configService);
-//   });
+  beforeEach(() => {
+    configService = { get: jest.fn(() => 'A,B,C') } as any;
+    service = new FeatureFlagService(configService);
+  });
 
-//   it('should return true if flag is enabled', () => {
-//     expect(service.isEnabled('A')).toBe(true);
-//     expect(service.isEnabled('B')).toBe(true);
-//     expect(service.isEnabled('C')).toBe(true);
-//   });
+  it('should return true if flag is enabled', () => {
+    expect(service.isEnabled('A')).toBe(true);
+    expect(service.isEnabled('B')).toBe(true);
+    expect(service.isEnabled('C')).toBe(true);
+  });
 
-//   it('should return false if flag is not enabled', () => {
-//     expect(service.isEnabled('D')).toBe(false);
-//   });
-// });
+  it('should return false if flag is not enabled', () => {
+    expect(service.isEnabled('D')).toBe(false);
+  });
+});
