0cf86bb7212791cee56dd8cbce295ede941ed03d
shamoo53 <aliyushamsu16@gmail.com>
2025-08-25 11:12:12 +0000

    ehnacement

--- src/configuration/tests/in-memory-cache.spec.ts
+++ src/configuration/tests/in-memory-cache.spec.ts
@@ -7,9 +7,44 @@ describe('InMemoryCache', () => {
     cache = new InMemoryCache();
   });
 
+
   it('should set and get values', () => {
     cache.set('foo', 'bar');
     expect(cache.get('foo')).toBe('bar');
+  });
+
+  it('should overwrite existing values', () => {
+    cache.set('foo', 'bar');
+    cache.set('foo', 'baz');
+    expect(cache.get('foo')).toBe('baz');
+  });
+
+  it('should support different value types', () => {
+    cache.set('num', 123);
+    cache.set('obj', { a: 1 });
+    expect(cache.get('num')).toBe(123);
+    expect(cache.get('obj')).toEqual({ a: 1 });
+  });
+
+  it('should report presence of keys', () => {
+    cache.set('foo', 'bar');
+    expect(cache.has('foo')).toBe(true);
+    expect(cache.has('missing')).toBe(false);
+  });
+
+  it('should track cache size', () => {
+    // @ts-ignore: access private for test
+    expect(cache.cache.size).toBe(0);
+    cache.set('a', 1);
+    cache.set('b', 2);
+    // @ts-ignore: access private for test
+    expect(cache.cache.size).toBe(2);
+    cache.delete('a');
+    // @ts-ignore: access private for test
+    expect(cache.cache.size).toBe(1);
+    cache.clear();
+    // @ts-ignore: access private for test
+    expect(cache.cache.size).toBe(0);
   });
 
   it('should return undefined for missing keys', () => {
