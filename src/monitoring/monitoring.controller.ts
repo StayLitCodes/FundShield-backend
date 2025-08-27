db16e6fac526bd9213699e189fd3740b7416407f
shamoo53 <aliyushamsu16@gmail.com>
2025-08-27 11:56:16 +0000

    made some adjustment

--- src/monitoring/monitoring.controller.ts
+++ src/monitoring/monitoring.controller.ts
@@ -25,22 +25,3 @@ export class MonitoringController {
     return this.metricsService.getRecentRequests(limit, route);
   }
 }
-import { Controller, Get } from '@nestjs/common';
-import { PerformanceMetricsService } from '../monitoring/performance-metrics.service';
-
-@Controller('monitoring')
-export class MonitoringController {
-  constructor(private readonly metricsService: PerformanceMetricsService) {}
-
-
-  @Get('metrics')
-  getMetrics(route?: string) {
-    return this.metricsService.getStats(route);
-  }
-
-
-  @Get('recent-requests')
-  getRecentRequests(limit?: number, route?: string): any {
-    return this.metricsService.getRecentRequests(limit, route);
-  }
-}
